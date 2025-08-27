72f37befa3addf8ce1f7f947b86cf1dcb08d9ccb
ffdadalto <ffdadalto@gmail.com>
2025-08-27 17:42:17 -0300

    add modelview

---
+++ AluGo/ModelViews/Utils/ModelValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluGo.ModelViews.Utils
+{   
+    public class EnumValidation : ValidationAttribute
+    {
+        public char NullValue { get; set; } = '0';
+
+        public override bool IsValid(object value)
+        {
+            if (!value.GetType().IsEnum)
+                return false;
+
+            var nullEnum = Enum.ToObject(value.GetType(), NullValue);
+
+            return nullEnum.ToString() != value.ToString();
+        }
+    }
+}
